a31c8a724e763ca9f4a92bba4a4e410a402784ec
Shivay00001 <164148410+Shivay00001@users.noreply.github.com>
2025-04-24 05:59:05 +0530

    Update root.jsx

--- root.jsx
+++ root.jsx
@@ -1,4 +1,3 @@
-// app/root.jsx
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 
 export default function App() {
@@ -7,6 +6,7 @@ export default function App() {
       <head>
         <Meta />
         <Links />
+        <title>Digital Immortality</title>
       </head>
       <body>
         <Outlet />
